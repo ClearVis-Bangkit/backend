bde60527b6ac24ccf710f69773cbdfd8fcc6545d
akmaldira <akmaldiraa@gmail.com>
2022-06-07 15:54:19 +0700

    articles update

--- controller/artikel.js
+++ controller/artikel.js
@@ -2,35 +2,70 @@ const cheerio = require("cheerio");
 const axios = require("axios");
 
 const getArticles = async() => {
-    const keyword = encodeURI('retinoblastoma');
-    const response = await axios.get(`https://news.google.com/search?for=${keyword}&hl=en-ID&gl=ID&ceid=ID%3Aen`, {
+    return await axios.get(`https://www.bing.com/news/search?q=retinoblastoma&qft=sortbydate%3d%221%22&form=YFNR`, {
         headers: {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36",
+            "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Mobile Safari/537.36",
          },
     })
-    const htmlData = response.data;
-    const $ = cheerio.load(htmlData);
-    const articels = []
-
-    $('article', htmlData).each(async(index, element) => {
-        const oldUrl = $(element).children('.VDXfz').attr('href');
-        const url = `https://news.google.com${oldUrl.replace('.', '')}`;
-        const title = $(element).children('.ipQwMb').text();
-        const postBy = $(element).children('.QmrVtf').children('.SVJrMe').children('.wEwyrc').text();
-        const datePost = $(element).children('.QmrVtf').children('.SVJrMe').children('time').text();
-        articels.push({
-            url,
-            title,
-            postBy,
-            datePost
+    .then(async({data}) => {
+        articles = []
+        const $ = cheerio.load(data)
+        $('div.newscard.vr', data).each((index, element) => {
+            const url = $(element).children('.caption').children('.title.itemlink').attr('href') || '';
+            const title = $(element).children('.caption').children('.title.itemlink').text() || '';
+            const imgOld = $(element).children('.image').children('a').children('img').attr('src');
+            let img = ''
+            imgOld !== undefined ? img = 'https://www.bing.com'+imgOld : img = 'No Image'
+            const postBy = $(element).children('.caption').children('.bottom').children('.source').children('a').text() || '';
+            const postDate = $(element).children('.caption').children('.bottom').children('span.timestamp').text() || '';
+            articles.push({
+                url,
+                title,
+                img,
+                postBy,
+                postDate,
+            })
         })
-    });
-    return articels;
+        if (articles.length < 10) {
+            await axios.get('https://www.bing.com/news/search?q=Eye+cancer&form=YFNR', {
+                headers: {
+                    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Mobile Safari/537.36",
+                 },
+            })
+            .then(({data}) => {
+                const $ = cheerio.load(data)
+                $('div.newscard.vr', data).each((index, element) => {
+                    const url = $(element).children('.caption').children('.title.itemlink').attr('href') || '';
+                    const title = $(element).children('.caption').children('.title.itemlink').text() || '';
+                    const imgOld = $(element).children('.image').children('a').children('img').attr('src');
+                    let img = ''
+                    imgOld !== undefined ? img = 'https://www.bing.com'+imgOld : img = 'No Image'
+                    const postBy = $(element).children('.caption').children('.bottom').children('.source').children('a').text() || '';
+                    const postDate = $(element).children('.caption').children('.bottom').children('span.timestamp').text() || '';
+                    articles.push({
+                        url,
+                        title,
+                        img,
+                        postBy,
+                        postDate,
+                    })
+                })
+            })
+        }
+        return articles
+    })
 }
 
 const getArticlesHandler = async(req, res) => {
-    const articles = await getArticles();
-    res.send(articles);
+    try {
+        const articles = await getArticles();
+        res.status(200).json({
+            success: true,
+            data: articles
+        });
+    } catch (err) {
+        res.status(500).json({success: false, msg: 'Server Errors'})
+    }
 } 
 
 module.exports = {
